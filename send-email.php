934b581058c5764882309c294054494c2254ef6c
Trey Greco <trey030303@gmail.com>
2024-06-03 14:14:50 -0400

    Finished contact form and php script

--- send-email.php
+++ send-email.php
@@ -1,33 +1,48 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$message = $_POST["message"];
-
-require "vendor/autoload.php";
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-$mail = new PHPMailer(true);
+require 'vendor/autoload.php';
 
-$mail->isSMTP();
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
-$mail->Host = "smtp.gmail.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
+if(isset($_POST['submit'])){
+	$name = $_POST['name'];
+	$from = $_POST['email'];
+	$phone = $_POST['phone'];
+	$messag = $_POST['message'];
 
-$mail->Username = "Trey@grecoclips.com";
-$mail->Password = "Asd4fghjkll5";
+	$mail = new PHPMailer(true);
 
-$mail->setFrom($email, $name);
-$mail->addAddress("Trey@grecoclips.com");
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'trey@grecoclips.com'; // Gmail address which you want to use as SMTP server
+	$mail->Password = 'ddwhrzefdhcalsrb'; // Gmail address Password
+	$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+	$mail->Port = 587;
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
 
-$mail->Subject = "New Message from Contact Form";
-$mail->isHTML(true);
-$mail->Body = "Name: " . $name . "<br>Email: " . $email . "<br>Phone: " . $phone . "<br>Message: " . $message;
+	$mail->setFrom($from);
 
-$mail->send();
+	$mail->addAddress('trey030303@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
-echo "Message has been sent";
+	$mail->Subject = 'New Message From '.$name;
+    $mail->Body = "Email: ".$from."\nPhone: ".$phone."\nMessage: ".$messag;
+
+    $result = $mail->send();
+
+	if ($result) {
+		header('Location: thankYou.html');
+	
+	}else{
+		echo '<script type="text/javascript">alert("Sorry! Message was not sent, Try again Later.");</script>';
+	 	echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+	}
+}
+
+?>
